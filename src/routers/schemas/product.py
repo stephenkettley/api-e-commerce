bb655aec6725f8a6ddfd309b1b53f17c41c3c9ff
Stephen Kettley <kettleystephen@gmail.com>
2023-04-19 16:27:32 +0200

    added one new product schema

--- src/routers/schemas/product.py
+++ src/routers/schemas/product.py
@@ -2,11 +2,22 @@ from pydantic import BaseModel
 
 
 class ProductBase(BaseModel):
-    """Pydantic model for showing all products."""
+    """Pydantic model for showing basic product information."""
 
     name: str
     price: float
     stock: int
+
+    class Config:
+        """ORM config class."""
+
+        orm_mode = True
+
+
+class ProductAll(ProductBase):
+    """Pydantic model for showing all products."""
+
+    id: int
 
     class Config:
         """ORM config class."""
